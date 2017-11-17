1999f178c324418d1c13dde49b8f96f68c14a72e
Karanpreet Singh <mailtokaranpreet@gmail.com>
2017-11-17 23:59:02 +0530

    Add index function

--- geetTodo/urls.py
+++ geetTodo/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^$', include('todos.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^todos/', include(todos.urls)),
+    url(r'^todos/', include('todos.urls')),
 ]
