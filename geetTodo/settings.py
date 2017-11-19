3ef2107f7175d95985a866db532efc3f09b12d10
Karanpreet Singh <mailtokaranpreet@gmail.com>
2017-11-19 11:04:29 +0530

    Configure mysql database

--- geetTodo/settings.py
+++ geetTodo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'geetTodo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todolist',
+        'USER': 'root',
+        'PASSWORD': 'karan0813',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
